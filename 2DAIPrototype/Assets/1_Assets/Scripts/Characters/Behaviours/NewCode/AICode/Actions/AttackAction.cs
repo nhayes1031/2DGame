fd81ac2d1da10eaa485e0fb7c56c9cd2bc714d88
Nicholas Hayes <nicholashayes@mail.weber.edu>
2019-10-17 08:57:27 -0400

    WIP - The enemy now tries to attack but it doesn't work

---
+++ 2DAIPrototype/Assets/1_Assets/Scripts/Characters/Behaviours/NewCode/AICode/Actions/AttackAction.cs
@@ -0,0 +1,16 @@
+﻿using Platformer.Scripts.Characters.Behaviours;
+using UnityEngine;
+
+namespace Platformer.Scripts.Characters.AI.Newcode
+{
+    [CreateAssetMenu(menuName = "AI/Actions/Attack")]
+    public class AttackAction : Action
+    {
+        private AttackCommand attackCom = new AttackCommand();
+
+        public override void Act(AiInputHandler controller)
+        {
+            attackCom.Execute(controller.gameObject);
+        }
+    }
+}
