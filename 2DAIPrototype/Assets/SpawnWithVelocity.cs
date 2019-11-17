07b59867e4815997ff43181ac4d2c3ec42198c72
Nicholas <nicholashayes@mail.weber.edu>
2019-11-17 15:32:25 -0500

    WIP-Bomb is refactored

--- 2DAIPrototype/Assets/SpawnWithVelocity.cs
+++ 2DAIPrototype/Assets/SpawnWithVelocity.cs
@@ -7,6 +7,6 @@ public class SpawnWithVelocity : MonoBehaviour
     public void Start()
     {
         Rigidbody2D rb2D = transform.GetComponent<Rigidbody2D>();
-        if (rb2D != null) rb2D.AddForce(Vector2.right * force, ForceMode2D.Impulse);
+        if (rb2D != null) rb2D.AddForce(transform.right * force, ForceMode2D.Impulse);
     }
 }
