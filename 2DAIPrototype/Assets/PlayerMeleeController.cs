0f4d22d94dcd0960a1f6c8e0d7ca1b3b2a0bc4ff
Nicholas <nicholashayes@mail.weber.edu>
2020-06-09 20:27:26 -0600

    Working on player dash

---
+++ 2DAIPrototype/Assets/PlayerMeleeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMeleeController : MonoBehaviour
+{
+    // Use the animator to start the Attack1 animation
+
+    // There should be an animation state that controls when the character is aiming or when they are melee attacking
+    // The aiming should only be allowed when the character is not melee attacking
+    // Only allow for the collider to be triggered when the character is melee attacking
+}
